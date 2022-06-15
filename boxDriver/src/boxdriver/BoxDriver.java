5d96587b0c3610fe9771955582091d1ac3b80bd6
Kevin Bell <bellKevin@pm.me>
2022-06-15 20:52:25 +0000

    Update BoxDriver.java

--- boxDriver/src/boxdriver/BoxDriver.java
+++ boxDriver/src/boxdriver/BoxDriver.java
@@ -3,6 +3,18 @@ package boxdriver;
 public class BoxDriver {
 
     public static void main(String[] args) {
-        
+        System.out.println("Ch 7 Rectangle Project by Kevin Bell\n");
+        Box paper = new Box(8, 11);
+        Box square = new Box(5, 5);
+        Box container = new Box(8, 4, 6);
+        Box cube = new Box(3, 3, 3);
+        System.out.println("Object paper");
+        paper.display();
+        System.out.println("\nObject square");
+        square.display();
+        System.out.println("\nObject container");
+        container.display();
+        System.out.println("\nObject cube");
+        cube.display();
     }
 }
